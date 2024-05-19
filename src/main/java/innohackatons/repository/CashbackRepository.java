ea94cfbdb3e9ab2b25b802cc5bcf3cab544d5805
Artur <arturmuxutdinov@gmail.com>
2024-05-19 10:11:03 +0300

    impl(project): refactor
    Implements: refactor

--- src/main/java/innohackatons/repository/CashbackRepository.java
+++ src/main/java/innohackatons/repository/CashbackRepository.java
@@ -1,10 +1,16 @@
 package innohackatons.repository;
 
+import innohackatons.entity.Bank;
 import innohackatons.entity.Cashback;
 import innohackatons.entity.CashbackId;
+import innohackatons.entity.Category;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CashbackRepository extends JpaRepository<Cashback, CashbackId> {
+    List<Cashback> findByBank(Bank bank);
+
+    Cashback findByBankAndCategory(Bank bank, Category category);
 }
